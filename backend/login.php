954b78afc402a72d1c4c93b47b17a4c95083292b
EarlySummerRain <46488780+EarlySummerRain@users.noreply.github.com>
2023-06-08 16:10:53 +0800

    feat: added login functionality

---
+++ backend/login.php
@@ -0,0 +1,56 @@
+<?php
+  session_start();
+
+  require_once "../config.php";
+
+  $username = filter_input(INPUT_POST, 'username');
+  $password = filter_input(INPUT_POST, 'password');
+  $hashedpassword = md5($password);
+
+  echo "<script type='text/javascript'>
+            window.alert('".$hashedpassword."');
+            window.location.href = '../index.php';
+        </script>";
+
+  $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+  if($conn === false){
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+  }
+
+  $query = "SELECT userid, uname, upassword FROM users WHERE uname=? AND upassword=?";
+  $stmt = $conn->prepare($query);
+  $stmt->bind_param("ss", $username, $hashedpassword);
+  $stmt->execute();
+  $result = $stmt->get_result();
+
+  $row = $result->fetch_assoc();
+
+  print_r($row);
+
+  if ($row) {
+
+    if ($row['uname'] == $username && $row['upassword'] == $hashedpassword) {
+      echo "<script type='text/javascript'>
+        window.alert('Login Successful. Redirecting..');
+        window.location.href = '../index.php';
+        </script>";
+      header('Location: ../html/dashboard.html');
+      $_SESSION['user_id'] = $row['userid'];
+        
+    } else {
+        echo "<script type='text/javascript'>
+            window.alert('Login Failed. Please Check Your Credentials');
+            window.location.href = '../index.php';
+        </script>";
+    }
+} else {
+    echo "<script type='text/javascript'>
+        window.alert('Login Failed. Please Check Your Credentials');
+        window.location.href = '../index.php';
+    </script>";
+}
+
+$conn->close();
+?>
+
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login - P2P</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="/group6_project127/css/styles.css"/>
+
+  </head>
+  <body>
+    <div class="body-html">
+      <nav>
+        <div class="nav-wrapper teal darken-4">
+          <a href="#" class="brand-logo">P2P</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="signup.php">Signup</a></li>
+            
+          </ul>
+        </div>
+      </nav>
+      <div class="page-content-login"> 
+        <div class="input-div">
+          <form method="POST" action="backend/login.php" onsubmit="return validation()">
+            <div >
+              <div class="input-field col s6">
+                <input placeholder="Username" id="username" name="username" type="text" class="validate">
+                <label for="username">Username</label>
+              </div>
+              <div class="input-field col s6">
+                <input placeholder="Password" id="password" name="password" type="text" class="validate">
+                <label for="password">Password</label>
+              </div>
+              <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+              </button>
+            </div>
+          </form>
+        </div>
+       
+  
+      </div>
+    </div>
+    
+    
+    <script>  
+      function validation()  
+      {  
+          let username=document.getElementById("username").value
+          let password=document.getElementById("password").value  
+          if(username.length=="" && password.length=="") {  
+              alert("User Name and Password fields are empty");  
+              return false;  
+          }  
+          else  
+          {  
+              if(username.length=="") {  
+                  alert("User Name is empty");  
+                  return false;  
+              }   
+              if (password.length=="") {  
+              alert("Password field is empty");  
+              return false;  
+              }  
+          }                             
+      }  
+    </script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
