1c05d5aaeaa8c890ac052f145a2db593d17f2c36
airanatividad <annatividad@up.edu.ph>
2023-06-09 00:01:25 +0800

    fix: show sidebar

--- pages/header.php
+++ pages/header.php
@@ -13,9 +13,12 @@ TO USE: include header.php ?> -->
   </style>
   <link rel="stylesheet" href="../css/styles.css">
   <link rel="stylesheet" href="../css/dashboard_style.css">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script></head>
 </head>
 <body>
   <div class="menu-btn">
--- pages/insert_expense_friend.php
+++ pages/insert_expense_friend.php
@@ -1,15 +1,56 @@
+<?php include('../config.php'); ?>
 <?php
     if(isset($_POST['add_expense_friend'])) {
         $e_name = $_POST['e_name'];
         $orig_amount = $_POST['orig_amount'];
-        $payer_uname = $_POST['f_payer_names'];
-        $friend_uname = $_POST['friend_names'];
+        $payer_id = $_POST['f_payer_names'];
+        $friend_id = $_POST['friend_names'];
         
         if($e_name == "" || empty($e_name) || 
-        $orig_amount == "" || empty($orig_amount) || 
-        $payer_uname == "" || empty($payer_uname) || 
-        $friend_uname == "" || empty($friend_uname)) {
+            $orig_amount == "" || empty($orig_amount) || 
+            $payer_id == "" || empty($payer_id) || 
+            $friend_id == "" || empty($friend_id)) {
             header('location:dashboard.php?friend_message=Please fill in all fields!');
+        } 
+
+        else {
+            $amount = $orig_amount / 2;
+
+            $dateQuery = "SELECT now()";
+            $dateResult = mysqli_query($mysqli, $dateQuery);
+            $date = mysqli_fetch_assoc($dateResult);
+
+            $current_auto_query = "SELECT `AUTO_INCREMENT` FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'p2p' AND TABLE_NAME = 'expenses'";
+            $current_auto_result = mysqli_query($mysqli, $current_auto_query);
+            $current_auto = mysqli_fetch_assoc($current_auto_result);
+
+            $query1 = "INSERT INTO `expenses` (`expense_type`, `expensename`, `date_incurred`, `original_amount`, `amount`, `payerid`, `groupid`) VALUES('friend', '$e_name', ".$date['now()'].", $orig_amount, $amount, '$payer_id', null)";
+            $query2 = "INSERT INTO `user_incurs_expense` (`userid`, `expenseid`) VALUES(".$_SESSION['user_id'].", ".$current_auto['AUTO_INCREMENT']."";
+            $query3 = "INSERT INTO `user_incurs_expense` (`userid`, `expenseid`) VALUES('$friend_id', ".$current_auto['AUTO_INCREMENT'].")";
+            
+            $result = mysqli_query($mysqli, $query1);
+            $result2 = mysqli_query($mysqli, $query2);
+            $result3 = mysqli_query($mysqli, $query3);
+
+            if(!$result && !$result2 && !$result3) {
+                die("Query Failed".mysqli_error());
+            }
+            else {
+                header('location:dashboard.php?insert_msg=Expense has been added successfully!');
+            }
+
+            // $date2 = mysqli_query($mysqli, $date);
+            // $date3 = mysqli_fetch_assoc($date2);
+            // // echo $date3['date'];
+
+            // $current_auto2 = mysqli_query($mysqli, $current_auto);
+            // $current_auto3 = mysqli_fetch_assoc($current_auto2);
+            //echo $amount .' - '. $date['date'] .' - '. $current_auto['AUTO_INCREMENT'];
+            //get expense id from $query1
+
+            // $query2 = "INSERT INTO `user_incurs_expense` (`userid`, `expenseid`) VALUES(001, $current_auto['AUTO_INCREMENT'])";
+            // $query3 = "INSERT INTO `user_incurs_expense` (`userid`, `expenseid`) VALUES(002, $current_auto['AUTO_INCREMENT'])";
+
         }
     }
 ?>
