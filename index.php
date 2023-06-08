c7227079457e17dabf9dab9816581424703bc625
jaymart-latigay <jglatigay@up.edu.ph>
2023-06-08 17:49:44 +0800

    ui: made a new styles file for login

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <title>Login - P2P</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <link rel="stylesheet" type="text/css" href="/group6_project127/css/styles.css"/>
+    <link rel="stylesheet" type="text/css" href="/group6_project127/css/loginstyles.css"/>
 
   </head>
   <body>
