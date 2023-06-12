b81181e096beb56e4fbd9f8ae9711432dd2895d3
EarlySummerRain <46488780+EarlySummerRain@users.noreply.github.com>
2023-06-13 00:29:29 +0800

    fix: delete group expense not deleting properly

--- backend/delete_group_expense.php
+++ backend/delete_group_expense.php
@@ -6,11 +6,11 @@
         $expense_id = $_GET['id'];
 
         // original amount of expenses - payments in that expenseid
-        $query1 = "SELECT original_amount-(SELECT sum(amount) 'amount' FROM `payments` where `expenseid` = '$expense_id') 'amount' FROM `expenses` where `expenseid` = $expense_id";
+        $query1 = "SELECT original_amount-(SELECT COALESCE(sum(amount),0) 'amount' FROM `payments` where `expenseid` = '$expense_id') 'amount' FROM `expenses` where `expenseid` = $expense_id";
         $result1 = mysqli_query($mysqli, $query1);
         $remaining_amount = mysqli_fetch_assoc($result1);
 
-        if (($remaining_amount['amount']) != 0) {
+        if (($remaining_amount['amount'] ?? 0) != 0) {
             header('location:../pages/dashboard.php?delete_g_message=Expense cannot be deleted due to unresolved payment.');
         } else {
             $query = "DELETE FROM `user_incurs_expense` where `expenseid` = '$expense_id'";
